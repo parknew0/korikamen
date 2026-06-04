3a409724cfce2f67a6c7965e9362a10be060f822
leehwanhun <127852445+nansis4457@users.noreply.github.com>
2026-06-04 21:20:54 +0900

    feat(stage3):  SpriteKitScene 바탕 UI구현

--- C3_Korikamen/Stages/Stage3_Spider/Stage3_TestView.swift
+++ C3_Korikamen/Stages/Stage3_Spider/Stage3_TestView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct Stage3_TestView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "arrow.forward")
+            .symbolEffect(.wiggle.byLayer, options: .repeat(.periodic(delay: 0.4)))
+            .font(.system(size: 50)) // 원하는 크기 지정
+            .foregroundColor(Color.white)
     }
 }
 
