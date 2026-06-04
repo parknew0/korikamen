0d07eb97fc8f6aac44977796d0541f046dc77521
leehwanhun <127852445+nansis4457@users.noreply.github.com>
2026-06-04 21:18:48 +0900

    feat(stage3):  SpriteKitScene 디자인 개선

---
+++ C3_Korikamen/Stages/Stage3_Spider/Stage3_TestView.swift
@@ -0,0 +1,18 @@
+//
+//  Stage3_TestView.swift
+//  C3_Korikamen
+//
+//  Created by 이환훈 on 6/4/26.
+//
+
+import SwiftUI
+
+struct Stage3_TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Stage3_TestView()
+}
