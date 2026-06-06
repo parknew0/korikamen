fd712814a8377bb21a887a53768c9b460881e36d
leehwanhun <127852445+nansis4457@users.noreply.github.com>
2026-06-06 21:36:02 +0900

    feat(story): 엔딩 씬 추가 및 스토리 에셋·구조 정리 - 엔딩 스토리(endingStory) 추가 + EndingView 스토리 엔진으로 전환 - 엔딩 배경 에셋(bg_ending1~9) 추가 - 스토리 에셋을 Story(Stage2) 하위로 폴더 재정리 - EndingView·MainView를 Story/Content로 이동 - StoryTransition을 StoryPage.swift로 통합, 미사용 Stage3_TestView 제거

--- C3_Korikamen/Story/Model/StoryPage.swift
+++ C3_Korikamen/Story/Model/StoryPage.swift
@@ -8,6 +8,11 @@
 import Foundation
 import SwiftUI
 
+//파일 정리
+enum StoryTransition { // 컷이 시작될 때 화면을 덮었다 걷히는 전환효과 종류들
+    case none
+    case fade(Color, TimeInterval) //색 시간 지정 페이드
+}
 
 //텍스트 박스
 struct StoryPanel {
